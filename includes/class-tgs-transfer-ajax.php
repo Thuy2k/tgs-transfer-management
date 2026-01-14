4ebec1fcc521c6200afba6689d61a2995df8482b
Thuy Nguyen Van <thuy.nguyenvan2000hn@gmail.com>
2026-01-14 16:40:00 +0700

    them tra hang noi bo

--- includes/class-tgs-transfer-ajax.php
+++ includes/class-tgs-transfer-ajax.php
@@ -923,6 +923,10 @@ class TGS_Transfer_Ajax
      */
     public static function approve_import()
     {
+        // DEBUG: Log để xác nhận hàm được gọi
+        error_log('=== TGS DEBUG: approve_import called ===');
+        error_log('POST data: ' . print_r($_POST, true));
+
         check_ajax_referer('tgs_transfer_nonce', 'nonce');
 
         global $wpdb;
